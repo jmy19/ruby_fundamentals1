d1929197b632a1c6c7dea9d2f649d5e0ec613218
jmy19 <jasonmyers@rogers.com>
2015-01-20 13:48:58 -0500

    excercise 4 complete fizzbuzz

---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+for number in(1..100)
+	if number % 3 == 0 && number % 5 == 0 
+		puts "BitMaker"
+	elsif number % 3 == 0 
+		puts "Bit"
+	elsif number % 5 == 0
+		puts "Maker"
+	else
+		puts number
+	end
+end
